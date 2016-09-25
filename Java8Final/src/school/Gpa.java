e62a02a642e49926c8072688de3cd1029ecbb6da
supaket.wongkampoo@outlook.com <supaket.wongkampoo@outlook.com>
2016-09-25 15:20:02 +0700

    1first state

---
+++ Java8Final/src/school/Gpa.java
@@ -0,0 +1,20 @@
+package school;
+
+import java.util.Scanner;
+
+
+public class Gpa {
+    
+    public static void main(String[] args) {
+    
+        System.out.println("Enter student number: ");
+        
+        //wait for input
+        Scanner scanner = new Scanner(System.in);
+        int numberOfStudent = scanner.nextInt();
+        
+        for( int i=0; i < numberOfStudent; i++) {
+          System.out.println("breka")  ;
+        }        
+    }
+}
